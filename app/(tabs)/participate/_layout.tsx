ee090f1fe0d4e86e72d065ac4ee082a898957f57
eziong <bsjwoong@gmail.com>
2023-10-12 20:14:43 +0900

    chore: absolute path

--- app/(tabs)/participate/_layout.tsx
+++ app/(tabs)/participate/_layout.tsx
@@ -1,7 +1,7 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { Stack } from "expo-router";
-import ParticipateHeader from "../../../src/components/participate/header";
+import ParticipateHeader from "@components/participate/header";
 
 type Props = {};
 
--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,25 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-    plugins: ['expo-router/babel'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      "expo-router/babel",
+      [
+        "module-resolver",
+        {
+          root: ["."],
+          extensions: [".ts", ".tsx", ".jsx", ".js", ".json"],
+          alias: {
+            "@app": "./app",
+            "@components": "./src/components",
+            "@models": "./src/models",
+            "@redux": "./src/redux",
+            "@screens": "./src/screens",
+            "@interfaces": "./src/interfaces",
+            "@utils": "./src/utils",
+          },
+        },
+      ],
+    ],
   };
 };
